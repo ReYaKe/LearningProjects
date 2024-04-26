fc0575cba66f0e81a7b0a2664fc2fad9d024136e
PenguGit <160494880+PenguGit@users.noreply.github.com>
2024-04-26 12:10:01 +0200

    Playing

--- Subnetting/Sub.py
+++ Subnetting/Sub.py
@@ -1,28 +1,44 @@
+import math
 ip = (input("Please input an IP-Adress: "))
 mask = (input("Please input a Subnetting-Mask: "))
-octchoice = input("Please input in which Octett your Subnet is: ")
-
+octchoice = int(input("Please input in which Octett your Subnet is: "))
 ipsplit = ip.split(".")
 masksplit = mask.split(".")
+x = 2
 print(ipsplit)
 print(masksplit)
-iploct = int(ipsplit[octchoice])
-maskloct = int(masksplit[octchoice])
+print(octchoice)
+if octchoice == 4:    
+    iploct = int(ipsplit[-1])
+    maskloct = int(masksplit[-1]) 
+    jump = 256 - maskloct
+    nIP = jump - 2
+    sbnr = int(iploct/jump)
+    nID = jump*sbnr
+    brdcst = nID + jump - 1
+    first = nID + 1
+    last = brdcst - 1
+elif octchoice == 3:
+    iploct = int(ipsplit[-2])
+    maskloct = int(masksplit[-2]) 
+    jump = 256 - maskloct
+    nIP = 2**(math.log(jump, 2) + 8)
+    sbnr = int(iploct/jump)
+    nID = jump*sbnr
+    brdcst = nID + jump - 1
+    first = nID + 1
+    last = brdcst - 1
+else:
+    print("No")
+    
 
 
 
-jump = 256 - maskloct
-nIP = jump - 2
-sbnr = int(iploct/jump)
-nID = jump*sbnr
-brdcst = nID + jump - 1
-first = nID + 1
-last = brdcst - 1
 
-print(jump)
-print(nIP)
-print(sbnr)
-print(nID)
-print(brdcst)
-print(first)
-print(last)
+print(f"Jump:{jump}")
+print(f"Host IPs:{nIP}")
+print(f"Subnumber:{sbnr}")
+print(f"Network-ID:{nID}")
+print(f"Broadcast-IP:{brdcst}")
+print(f"First Host IP:{first}")
+print(f"Last Host IP:{last}")
