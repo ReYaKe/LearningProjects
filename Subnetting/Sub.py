1411f60bd8ef1067f178d93fccb633445d027ed7
PenguGit <160494880+PenguGit@users.noreply.github.com>
2024-04-26 14:15:09 +0200

    Redid all of it I guess

--- Subnetting/Sub.py
+++ Subnetting/Sub.py
@@ -1,61 +1,51 @@
 import math
-ip = (input("Please input an IP-Adress: "))
-mask = (input("Please input a Subnetting-Mask: "))
-octchoice = int(input("Please input in which Octett your Subnet is: "))
-ipsplit = ip.split(".")
-masksplit = mask.split(".")
-x = 2
-print(ipsplit)
-print(masksplit)
-print(octchoice)
-if octchoice == 4:    
-    iploct = int(ipsplit[-1])
-    maskloct = int(masksplit[-1]) 
-    jump = 256 - maskloct
-    nIP = jump - 2
-    sbnr = int(iploct/jump)
-    nID = jump*sbnr
-    brdcst = nID + jump - 1
-    first = nID + 1
-    last = brdcst - 1
-elif octchoice == 3:
-    iploct = int(ipsplit[-2])
-    maskloct = int(masksplit[-2]) 
-    jump = 256 - maskloct
-    nIP = 2**(math.log(jump, 2) + 8)
-    sbnr = int(iploct/jump)
-    nID = jump*sbnr
-    brdcst = nID + jump - 1
-    first = nID + 1
-    last = brdcst - 1
-elif octchoice == 2:
-    iploct = int(ipsplit[-3])
-    maskloct = int(masksplit[-3]) 
-    jump = 256 - maskloct
-    nIP = 2**(math.log(jump, 2) + 16)
-    sbnr = int(iploct/jump)
-    nID = jump*sbnr
-    brdcst = nID + jump - 1
-    first = nID + 1
-    last = brdcst - 1
-elif octchoice == 1:
-    iploct = int(ipsplit[-4])
-    maskloct = int(masksplit[-4]) 
-    jump = 256 - maskloct
-    nIP = 2**(math.log(jump, 2) + 24)
-    sbnr = int(iploct/jump)
-    nID = jump*sbnr
-    brdcst = nID + jump - 1
-    first = nID + 1
-    last = brdcst - 1
-else:
-    print("No")
-    
 
-print(f"Jump:{jump}")
-print(f"Host IPs:{nIP}")
-print(f"Subnumber:{sbnr}")
-print(f"Network-ID:{nID}")
-print(f"Broadcast-IP:{brdcst}")
-print(f"First Host IP:{first}")
-print(f"Last Host IP:{last}")
+def subnet_calc(ip,mask,octchoice):   
+    ipsplit = ip.split(".")
+    masksplit = mask.split(".")
+    iploct = int(ipsplit[octchoice-1])
+    maskloct = int(masksplit[octchoice-1])
+    jump = 256 - maskloct
+    nIP = 2 ** (math.log(jump, 2) + (8 * (4 - octchoice)))
+    sbnr = int(iploct/jump)
+    nID = jump*sbnr
+    if octchoice != 4:
+        last_2 = nID + jump-1
+        first = 0
+        brdcst = 255
+        last = 254
+        subnet_details = {
+        "Original IP": ip,
+        "Subnet ID": ".".join(ipsplit[:octchoice - 1] + [str(nID)] + ipsplit[octchoice:]),
+        "First usable IP": ".".join(ipsplit[:octchoice-1] + [str(nID)] + [str(first)]),
+        "Last usable IP":".".join(ipsplit[:octchoice-1] + [str(last_2)] + [str(last)]),
+        "Broadcast IP":".".join(ipsplit[:octchoice-1] + [str(last_2)] + [str(brdcst)]),
+        "Total usable IPs in subnet": int(nIP),
+        }
+         
+    else:
+        first = nID + 1
+        brdcst = nID + jump -1
+        last = brdcst - 1
+        subnet_details = {
+        "Original IP": ip,
+        "Subnet ID": ".".join(ipsplit[:octchoice - 1] + [str(nID)] + ipsplit[octchoice:]),
+        "First usable IP": ".".join(ipsplit[:octchoice - 1] + [str(first)] + ipsplit[octchoice:]),
+        "Last usable IP":".".join(ipsplit[:octchoice - 1] + [str(last)] + ipsplit[octchoice:]),
+        "Broadcast IP":".".join(ipsplit[:octchoice - 1] + [str(brdcst)] + ipsplit[octchoice:]),
+        "Total usable IPs in subnet": int(nIP),
+        }
+    return subnet_details
+
+
+"""ip = (input("Please input an IP-Adress: "))
+mask = (input("Please input a Subnetting-Mask: "))
+octchoice = int(input("Please input in which Octett your Subnet is: "))"""
+ip = "10.0.212.0"
+mask = "255.255.248.0"
+octchoice = 3
+subnet_details = subnet_calc(ip,mask,octchoice)
+
+
+for key, value in subnet_details.items():
+    print(f'{key}: {value}')
